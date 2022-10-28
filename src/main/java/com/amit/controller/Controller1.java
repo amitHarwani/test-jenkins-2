4c1c5ebe87b04deda62339e4df0926fc527ff902
amitHarwani <harwania21@gmail.com>
2022-10-28 16:10:46 +0400

    Update 1

--- src/main/java/com/amit/controller/Controller1.java
+++ src/main/java/com/amit/controller/Controller1.java
@@ -2,6 +2,7 @@ package com.amit.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class Controller1 {
 	
 	
 
-	@GetMapping("greet/{message}")
+	@PostMapping("/{message}")
 	public String greet(@PathVariable String message) {
 		return "Welcome: "+message;
 	}
