df40a3059751e81581bc44b16a6cd7f0deb1fb92
amitHarwani <harwania21@gmail.com>
2022-10-28 16:31:29 +0400

    Update 3

--- src/main/java/com/amit/controller/Controller1.java
+++ src/main/java/com/amit/controller/Controller1.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("c1")
+@RequestMapping("/")
 public class Controller1 {
 
 	@GetMapping
